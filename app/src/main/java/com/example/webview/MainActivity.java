c9f44b7bd08072622632e4af4bc854ee7121386f
tapasm569 <tapasm569@gmail.com>
2026-09-12 16:06:27 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -5,7 +5,6 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.webview.R;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
