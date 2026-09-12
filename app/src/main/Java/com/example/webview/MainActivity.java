6a84f50bc479093d8eed3710153401819cf29c91
tapasm569 <tapasm569@gmail.com>
2026-09-12 11:33:10 +0530

    Set app theme in MainActivity on creation
    Set the app theme to Theme_AppCompat_Light_NoActionBar in MainActivity.

--- app/src/main/Java/com/example/webview/MainActivity.java
+++ app/src/main/Java/com/example/webview/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_AppCompat_Light_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -37,6 +38,3 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
-}
-    }
-            }
